60efd8f082adc9d32234dc77722fe7bbfe215ba7
dpound@naver.com <dpound@naver.com>
2024-12-17 23:38:42 +0900

    가게등록 endpoint 등록 어노테이션 추가

--- src/main/java/com/neighborcouponbook/controller/ShopController.java
+++ src/main/java/com/neighborcouponbook/controller/ShopController.java
@@ -1,8 +1,14 @@
 package com.neighborcouponbook.controller;
 
+import com.neighborcouponbook.common.annotation.MenuInformation;
 import com.neighborcouponbook.model.Shop;
+import com.neighborcouponbook.model.vo.MenuVo;
 import com.neighborcouponbook.model.vo.ShopVo;
 import com.neighborcouponbook.service.ShopService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +23,27 @@ import java.util.List;
 public class ShopController {
 
     private final ShopService shopService;
-    
-    /*
-    * 가게 등록
-    * userId, shopTypeId 가져와서 입력
-    * */
+
+    @Operation(
+            summary = "가게등록",
+            description = "userId, shopTypeId 가져와서 입력",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = ShopVo.class))
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "조회 성공",
+                            useReturnTypeSchema = true
+                    )
+            }
+    )
+    @MenuInformation(
+            menuAuthDetail = {
+                    @MenuInformation.MenuRoleDetail(rolesName = "super-admin", roleId = 1),
+                    @MenuInformation.MenuRoleDetail(rolesName = "user", roleId = 2)
+            })
     @PostMapping(value = "create")
     public ResponseEntity<?> createShop(@RequestBody ShopVo shopVo) {
         return shopService.createShop(shopVo);
